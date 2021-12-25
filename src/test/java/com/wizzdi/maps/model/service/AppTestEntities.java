091b717b2ce07ad201511018492460cfc411945a
Admin <Admin@wizzdiCloud.com>
2021-12-25 21:19:01 +0200

    created:

--- src/test/java/com/wizzdi/maps/model/service/AppTestEntities.java
+++ src/test/java/com/wizzdi/maps/model/service/AppTestEntities.java
@@ -29,6 +29,42 @@ import com.flexicore.model.territories.Country;
 import com.flexicore.model.territories.Neighbourhood;
 import com.flexicore.model.territories.State;
 import com.flexicore.model.territories.Street;
+import com.flexicore.organization.model.Branch;
+import com.flexicore.organization.model.Consumer;
+import com.flexicore.organization.model.ConsumerToOrganization;
+import com.flexicore.organization.model.Customer;
+import com.flexicore.organization.model.CustomerDocument;
+import com.flexicore.organization.model.Employee;
+import com.flexicore.organization.model.IndividualCustomer;
+import com.flexicore.organization.model.Industry;
+import com.flexicore.organization.model.IndustryToCustomer;
+import com.flexicore.organization.model.Organization;
+import com.flexicore.organization.model.OrganizationalCustomer;
+import com.flexicore.organization.model.SalesPerson;
+import com.flexicore.organization.model.SalesPersonToRegion;
+import com.flexicore.organization.model.SalesRegion;
+import com.flexicore.organization.model.Site;
+import com.flexicore.organization.model.Supplier;
+import com.flexicore.organization.model.SupplierApi;
+import com.viewpark.pricing.model.Currency;
+import com.viewpark.pricing.model.OccasionalPrice;
+import com.viewpark.pricing.model.OccasionalPriceItem;
+import com.viewpark.pricing.model.ParkingLotToPriceList;
+import com.viewpark.pricing.model.Price;
+import com.viewpark.pricing.model.PriceList;
+import com.viewpark.pricing.model.PriceListType;
+import com.viewpark.pricing.model.SubscriptionPrice;
+import com.viewpark.pricing.model.Voucher;
+import com.viewpark.pricing.model.VoucherType;
+import com.viewpark.pricing.model.VoucherTypeToOccasionalPrice;
+import com.viewpark.subscriptions.model.Car;
+import com.viewpark.subscriptions.model.ParkingUser;
+import com.viewpark.subscriptions.model.Subscription;
+import com.viewpark.subscriptions.model.SubscriptionItem;
+import com.viewpark.subscriptions.model.SubscriptionToCar;
+import com.viewpark.subscriptions.model.SubscriptionToParkingArea;
+import com.viewpark.subscriptions.model.SubscriptionToParkingLot;
+import com.viewpark.subscriptions.model.SubscriptionToParkingSpace;
 import com.wizzdi.dynamic.properties.converter.JsonConverter;
 import com.wizzdi.flexicore.boot.jpa.service.EntitiesHolder;
 import com.wizzdi.flexicore.file.model.FileResource;
@@ -41,6 +77,17 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import parking.gear.model.Building;
+import parking.gear.model.BuildingFloor;
+import parking.gear.model.Camera;
+import parking.gear.model.Gate;
+import parking.gear.model.LPREngine;
+import parking.gear.model.Lane;
+import parking.gear.model.ParkingArea;
+import parking.gear.model.ParkingLot;
+import parking.gear.model.ParkingSpace;
+import parking.gear.model.ParkingSpaceState;
+import parking.gear.model.ParkingSpaceToParkingSpaceState;
 
 @Configuration
 public class AppTestEntities {
@@ -51,14 +98,61 @@ public class AppTestEntities {
     return new EntitiesHolder(
         new HashSet<>(
             Arrays.asList(
+                Building.class,
+                IndustryToCustomer.class,
                 Neighbourhood.class,
+                OrganizationalCustomer.class,
                 Street.class,
+                Site.class,
+                SalesRegion.class,
+                Consumer.class,
+                SalesPersonToRegion.class,
+                SalesPerson.class,
                 State.class,
                 City.class,
+                CustomerDocument.class,
+                Branch.class,
+                SupplierApi.class,
+                Organization.class,
+                IndividualCustomer.class,
+                Employee.class,
+                Supplier.class,
+                Customer.class,
+                Industry.class,
+                ConsumerToOrganization.class,
                 Address.class,
+                ParkingSpace.class,
+                ParkingArea.class,
+                ParkingLot.class,
                 MappedPOI.class,
+                Lane.class,
+                Camera.class,
                 Country.class,
+                BuildingFloor.class,
+                Gate.class,
+                Currency.class,
+                ParkingSpaceToParkingSpaceState.class,
+                ParkingSpaceState.class,
+                PriceListType.class,
+                PriceList.class,
                 Baseclass.class,
+                ParkingLotToPriceList.class,
+                SubscriptionPrice.class,
+                Price.class,
+                OccasionalPrice.class,
+                Subscription.class,
+                Voucher.class,
+                VoucherTypeToOccasionalPrice.class,
+                VoucherType.class,
+                OccasionalPriceItem.class,
+                SubscriptionItem.class,
+                LPREngine.class,
+                ParkingUser.class,
+                Car.class,
+                SubscriptionToParkingArea.class,
+                SubscriptionToCar.class,
+                SubscriptionToParkingSpace.class,
+                SubscriptionToParkingLot.class,
                 OperationToClazz.class,
                 SecurityTenant.class,
                 PermissionGroup.class,
