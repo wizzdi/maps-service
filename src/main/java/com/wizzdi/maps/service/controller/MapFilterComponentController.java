7f14ed163447685e3b1a646a273cb2906eae97f0
Avishay Ben Natan <avishaybennatan@gmail.com>
2022-09-27 08:34:05 +0300

    Added an API to get all possible values for FilterComponentType

--- src/main/java/com/wizzdi/maps/service/controller/MapFilterComponentController.java
+++ src/main/java/com/wizzdi/maps/service/controller/MapFilterComponentController.java
@@ -4,6 +4,7 @@ import com.flexicore.annotations.OperationsInside;
 import com.flexicore.security.SecurityContextBase;
 import com.wizzdi.flexicore.boot.base.interfaces.Plugin;
 import com.wizzdi.flexicore.security.response.PaginationResponse;
+import com.wizzdi.maps.service.request.FilterComponentType;
 import com.wizzdi.maps.service.request.MapFilterComponentRequest;
 import com.wizzdi.maps.service.request.MappedPOIFilter;
 import com.wizzdi.maps.service.response.MapFilterComponent;
@@ -14,6 +15,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.pf4j.Extension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("MapFilterComponent")
@@ -34,5 +39,14 @@ public class MapFilterComponentController implements Plugin {
             @RequestAttribute SecurityContextBase securityContext) {
         mappedPOIService.validate(mapFilterComponentRequest, securityContext);
         return mappedPOIService.getAllMapFilterComponents(mapFilterComponentRequest, securityContext);
+    }
+    @Operation(summary = "get all possible values for FilterComponentType")
+    @GetMapping("getAllMapFilterComponentsTypes")
+    public List<String> getAllMapFilterComponentsTypes(
+            @RequestHeader("authenticationKey") String authenticationKey,
+
+            @RequestAttribute SecurityContextBase securityContext) {
+
+       return Arrays.stream(FilterComponentType.values()).map(f->f.toString()).collect(Collectors.toList());
     }
 }
--- src/main/java/com/wizzdi/maps/service/service/MapFilterComponentService.java
+++ src/main/java/com/wizzdi/maps/service/service/MapFilterComponentService.java
@@ -6,6 +6,7 @@ import com.wizzdi.flexicore.boot.base.interfaces.Plugin;
 import com.wizzdi.flexicore.security.response.PaginationResponse;
 import com.wizzdi.maps.service.data.MapFilterComponentRepository;
 import com.wizzdi.maps.service.request.FilterComponentPropertyProvider;
+import com.wizzdi.maps.service.request.FilterComponentType;
 import com.wizzdi.maps.service.request.MapFilterComponentRequest;
 import com.wizzdi.maps.service.request.StandardFilterComponentProvider;
 import com.wizzdi.maps.service.response.MapFilterComponent;
@@ -14,11 +15,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.env.ConfigTreePropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.Tuple;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -41,7 +44,8 @@ public class MapFilterComponentService implements Plugin {
         }
         if(!mapFilterComponentRequest.isCustom()){
             if(mapFilterComponentRequest.getFilterComponentType()==null){
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"filter component type must be provided");
+                String possibleValues = Arrays.stream(FilterComponentType.values()).map(f -> f.toString()).collect(Collectors.joining(","));
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"a single filter component type must be provided: "+possibleValues);
             }
             mapFilterComponentRequest.setFilterComponentPropertyProvider(new StandardFilterComponentProvider());
         }
