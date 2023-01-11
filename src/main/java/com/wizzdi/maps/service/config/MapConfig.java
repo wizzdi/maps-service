1a10a857c37b19935aa374d38f8b5523df95e11d
Asaf Ben Natan <asaf@wizzdi.com>
2023-01-12 00:03:54 +0200

    added transaction management

--- src/main/java/com/wizzdi/maps/service/config/MapConfig.java
+++ src/main/java/com/wizzdi/maps/service/config/MapConfig.java
@@ -4,9 +4,11 @@ import com.wizzdi.flexicore.boot.base.interfaces.Plugin;
 import org.pf4j.Extension;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableAsync(proxyTargetClass = true)
 @Extension
+@EnableTransactionManagement(proxyTargetClass = true)
 public class MapConfig implements Plugin {
 }
